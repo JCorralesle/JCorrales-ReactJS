1da1c0cfada787ac1eb270852675e0e9f50f8ba1
jcorrales <jcorrales.ica@gmail.com>
2022-07-07 19:04:34 -0500

    app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coderhouse - Julio Corrales
         </p>
         <a
           className="App-link"
