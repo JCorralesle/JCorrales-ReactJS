229330b20704f32e856b9c02d5c891e358e8be3c
JCorralesle <jcorrales.ica@gmail.com>
2022-07-19 16:05:13 -0500

    Contador

---
+++ src/Components/ItemCount/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({stock}) => {
+    const [quantity, SetQuantity] = useState(1)
+
+    const agregarCantidad = () => {
+        if (quantity < stock){
+            SetQuantity(quantity + 1)
+        }
+    }
+
+    const disminuirCantidad = () => {
+        if (quantity > 1){
+            SetQuantity(quantity - 1)
+        }
+    }
+
+    const handleAddCart = () => {
+        alert(`Se agregan ${quantity} productos al carrito`);
+    }
+
+    return (
+        <div>
+            <button onClick={agregarCantidad}> + </button> 
+            <span>{quantity}</span>
+            <button onClick={disminuirCantidad}> - </button>
+            <br/>
+            <button onClick={handleAddCart} > Agregar al Carrito </button>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/Container/ItemListContainer/index.jsx
+++ src/Container/ItemListContainer/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ItemCount from '../../Components/ItemCount'
 
 const ItemListContainer = ({greeting} ) => {
   return (
     <div>
         <h1> {greeting} </h1>
+        <ItemCount stock={10} />
     </div>
   )
 }
